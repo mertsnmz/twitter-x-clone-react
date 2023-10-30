b40e2367993fea73d1e872c88907f9765ab4039c
mertsnmz <69246848+mertsnmz@users.noreply.github.com>
2023-10-30 14:50:37 +0300

    sidebar menu created

--- src/utils/consts.jsx
+++ src/utils/consts.jsx
@@ -1,3 +1,7 @@
+import store from "~/store"
+
+const states = store.getState()
+
 export const mainMenu = [
     {
         path: '/',
@@ -142,7 +146,7 @@ export const mainMenu = [
         },
     },
     {
-        path: '/profile',
+        path: `/${states?.auth?.currentAccount?.username}`,
         title: 'Profil',
         icon: {
             active: (
